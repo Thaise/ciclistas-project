6fd329f1e5faf11b7e9a5f5fc0f001717b19ab34
Thaise <thaisefloripa@gmail.com>
2011-10-26 20:01:01 -0200

    Update test/CiclistaTest.java

--- test/CiclistaTest.java
+++ test/CiclistaTest.java
@@ -50,48 +50,48 @@ public class CiclistaTest {
 	@Test
 	public void deveCalcularMedia1() {
 		double distancia = 20;
-		double velocidade = 2;
-		double media = Ciclista.calcularVelocidadeMedia(distancia,velocidade);
+		double tempo = 2;
+		double media = Ciclista.calcularVelocidadeMedia(distancia,tempo);
 		assertEquals(10,media,0);	
 	}
 	
 	@Test
 	public void deveCalcularMedia2() {
 		double distancia = 40;
-		double velocidade = 2.5;
-		double media = Ciclista.calcularVelocidadeMedia(distancia,velocidade);
+		double tempo = 2.5;
+		double media = Ciclista.calcularVelocidadeMedia(distancia,tempo);
 		assertEquals(16,media,0);	
 	}
 	
 	@Test
 	public void deveCalcularMedia3() {
 		double distancia = 30;
-		double velocidade = 3.6;
-		double media = Ciclista.calcularVelocidadeMedia(distancia,velocidade);
+		double tempo = 3.6;
+		double media = Ciclista.calcularVelocidadeMedia(distancia,tempo);
 		assertEquals(8.0,media,0);	
 	}
 	
 	@Test
 	public void deveCalcularMedia4() {
 		double distancia = 100;
-		double velocidade = 50;
-		double media = Ciclista.calcularVelocidadeMedia(distancia,velocidade);
+		double tempo = 50;
+		double media = Ciclista.calcularVelocidadeMedia(distancia,tempo);
 		assertEquals(2,media,0);	
 	}
 	
 	@Test
 	public void deveCalcularMedia5() {
 		double distancia = 36;
-		double velocidade = 6;
-		double media = Ciclista.calcularVelocidadeMedia(distancia,velocidade);
+		double tempo = 6;
+		double media = Ciclista.calcularVelocidadeMedia(distancia,tempo);
 		assertEquals(6,media,0);	
 	}
 	
 	@Test
 	public void deveCalcularMedia6() {
 		double distancia = 64;
-		double velocidade = 8;
-		double media = Ciclista.calcularVelocidadeMedia(distancia,velocidade);
+		double tempo = 8;
+		double media = Ciclista.calcularVelocidadeMedia(distancia,tempo);
 		assertEquals(8,media,0);	
 	}
 	
