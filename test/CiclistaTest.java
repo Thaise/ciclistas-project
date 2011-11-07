ef34bc2458117b6c6c81841a41bfd05c91d89339
Thaise <thaisefloripa@gmail.com>
2011-11-07 21:59:16 -0200

    Avaliação "Ciclistas"

--- test/CiclistaTest.java
+++ test/CiclistaTest.java
@@ -5,85 +5,85 @@ import org.junit.Test;
 public class CiclistaTest {
 
 	@Test
-	public void deveInterpretarDistanciaEVelocidade() throws CiclistaException{
+	public void deveInterpretarDistancia20EVelocidade2() throws CiclistaException{
 		String valores = "20 2";
 		double[] valoresInterpretados = Ciclista.interpretarValoresDeDistanciaTempo(valores);
 		assertArrayEquals(new double[] {20,2},valoresInterpretados,0);	
 	}
 	
 	@Test
-	public void deveInterpretarDistanciaEVelocidade2() throws CiclistaException {
-		String valores = "40 2.5";
+	public void deveInterpretarDistancia40EVelocidade2() throws CiclistaException {
+		String valores = "40 2";
 		double[] valoresInterpretados = Ciclista.interpretarValoresDeDistanciaTempo(valores);
-		assertArrayEquals(new double[] {40.0, 2.5},valoresInterpretados,0);	
+		assertArrayEquals(new double[] {40.0, 2},valoresInterpretados,0);	
 	}
 	
 	@Test
-	public void deveInterpretarDistanciaEVelocidade3() throws CiclistaException {
-		String valores = "30 3.6";
+	public void deveInterpretarDistancia30EVelocidade3() throws CiclistaException {
+		String valores = "30 3";
 		double[] valoresInterpretados = Ciclista.interpretarValoresDeDistanciaTempo(valores);
-		assertArrayEquals(new double[] {30.0, 3.6},valoresInterpretados,0);	
+		assertArrayEquals(new double[] {30.0, 3},valoresInterpretados,0);	
 	}
 	
 	
 	@Test
-	public void deveInterpretarDistanciaEVelocidade4() throws CiclistaException {
+	public void deveInterpretarDistancia105EVelocidade50() throws CiclistaException {
 		String valores = "105 50";
 		double[] valoresInterpretados = Ciclista.interpretarValoresDeDistanciaTempo(valores);
 		assertArrayEquals(new double[] {105, 50},valoresInterpretados,0);	
 	}
 	
 	@Test
-	public void deveTestarConversaoMetrosParaKMeSparaH(){
-		double[] media = Ciclista.converterParaPadrao("m", "s", 3000, 3600);
-		assertArrayEquals(new double[] {3, 1},media,0);	
+	public void deveTestarConversao3000MetrosPara3KM(){
+		double media = Ciclista.converterDistanciaParaPadrao(Ciclista.METROS, 3000);
+		assertEquals(3 , media, 0);	
 	}
 	
 	@Test
-	public void deveTestarConversaoMetrosParaKMeMparaH(){
-		double[] media = Ciclista.converterParaPadrao("m", "m", 3000, 120);
-		assertArrayEquals(new double[] {3, 2},media,0);	
+	public void deveTestarConversao120Minutospara2Horas(){
+		double media = Ciclista.converterTempoParaPadrao(Ciclista.MINUTOS, 120);
+		assertEquals(2 , media, 0);	
 	}
 	
 	@Test
-	public void deveTestarConversaoKMparaMS(){
-		double[] distETempo = {3,1};
-		double[] media = Ciclista.converterDistanciasETempos(distETempo,"ms");
-		assertArrayEquals(new double[] {3000, 3600},media,0);	
+	public void deveTestarConversao3600Segundospara1Hora(){
+		double media = Ciclista.converterTempoParaPadrao(Ciclista.SEGUNDOS,3600);
+		assertEquals(1, media, 0);	
 	}
 	
 	@Test
-	public void deveTestarConversaoKMparaKM(){
-		double[] distETempo = {3,1};
-		double[] media = Ciclista.converterDistanciasETempos(distETempo, "KM");
-		assertArrayEquals(new double[] {3, 1},media,0);	
+	public void deveTestarConversao3KMpara3000Metros(){
+		double media = Ciclista.converterDistanciaConformeVelocidadeEscolhida(3,Ciclista.METROSPORSEGUNDO);
+		assertEquals(3000, media, 0);	
 	}
 	
 	@Test
-	public void deveCalcularMedia1() throws CiclistaException {
-		double[] distETempo = {20,10};
-		double media = Ciclista.calcularVelocidadeMedia(distETempo);
+	public void deveTestarConversao2Horaspara7200Segundos(){
+		double media = Ciclista.converterTempoConformeVelocidadeEscolhida(2,Ciclista.METROSPORSEGUNDO);
+		assertEquals(7200, media, 0);	
+	}
+	
+	@Test
+	public void deveCalcularVelocidadeMediaDeDistancia20ETempo10() throws CiclistaException {
+		double media = Ciclista.calcularVelocidadeMedia(20, 10);
 		assertEquals(2,media,0);	
 	}
 	
 	@Test
-	public void deveCalcularMedia2() throws CiclistaException {
-		double[] distETempo = {40,5};
-		double media = Ciclista.calcularVelocidadeMedia(distETempo);
+	public void deveCalcularVelocidadeMediaDeDistancia40ETempo5() throws CiclistaException {
+		double media = Ciclista.calcularVelocidadeMedia(40,5);
 		assertEquals(8,media,0);	
 	}
 	
 	@Test
-	public void deveCalcularMedia3() throws CiclistaException {
-		double[] distETempo = {62,3};
-		double media = Ciclista.calcularVelocidadeMedia(distETempo);
-		assertEquals(21,media,0);	
+	public void deveCalcularVelocidadeMediaDeDistancia10ETempo2() throws CiclistaException {
+		double media = Ciclista.calcularVelocidadeMedia(10,2);
+		assertEquals(5,media,0);	
 	}
 	
 	@Test
-	public void deveCalcularMedia4() throws CiclistaException {
-		double[] distETempo = {54,9};
-		double media = Ciclista.calcularVelocidadeMedia(distETempo);
+	public void deveCalcularVelocidadeMediaDeDistancia54ETempo9() throws CiclistaException {
+		double media = Ciclista.calcularVelocidadeMedia(54,9);
 		assertEquals(6,media,0);	
 	}	
 
